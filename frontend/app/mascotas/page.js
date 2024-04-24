149c43bc3c3c0f125101dae6da13b3a025786ddd
Cristina Álvarez López <elo.programacion@gmail.com>
2024-04-24 11:29:07 +0200

    fixing some bug

--- frontend/app/mascotas/page.js
+++ frontend/app/mascotas/page.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const page = () => {
+export default function page  () {
   return (
     <div>aqui la protectora ve sus mascotas disponibles, las adoptadas y un botón para dar nueva mascotas de alta</div>
   )
