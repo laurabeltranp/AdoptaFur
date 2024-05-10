1b127ae8d4d5000525370b999f079ab09de7f69b
Cristina Álvarez López <elo.programacion@gmail.com>
2024-05-10 22:41:05 +0200

    magic

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -3,9 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./globals.css";
 import NavBar from '../components/navbar/navbar.js';
 import { Footer } from "@/components/footer/footer";
-import Script from "next/script";
 import {UsuarioProvider} from "@/contexts/UsuarioContext";
-
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,9 +14,9 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <UsuarioProvider>
           <NavBar></NavBar>
-        </UsuarioProvider>
           {children}
           <Footer></Footer>
+        </UsuarioProvider>
       </body>
     </html>
   );
