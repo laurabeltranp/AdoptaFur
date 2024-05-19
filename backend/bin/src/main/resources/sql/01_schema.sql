ff9c1aeee6a13f16a7b1ddd643488365decf2de7
Laura <laurabeltranp15@gmail.com>
2024-05-19 18:24:06 +0200

    navbar y mas

---
+++ backend/bin/src/main/resources/sql/01_schema.sql
@@ -0,0 +1,79 @@
+CREATE DATABASE  IF NOT EXISTS `adoptafur`;
+USE `adoptafur`;
+
+SET FOREIGN_KEY_CHECKS=0;
+
+DROP TABLE IF EXISTS `roles`;
+CREATE TABLE `roles`
+(
+    `idrol`  int NOT NULL AUTO_INCREMENT,
+    `nombre` varchar(255) NOT NULL,
+    PRIMARY KEY (`idrol`)
+);
+
+DROP TABLE IF EXISTS `mascotas`;
+CREATE TABLE `mascotas`
+(
+    `idmascota`   int NOT NULL AUTO_INCREMENT,
+    `estado`      enum('DISPONIBLE','BLOQUEADA', 'ADOPTADA') NOT NULL,
+    `cumpleanio`  date         NOT NULL,
+    `description` varchar(255) NOT NULL,
+    `foto`        mediumblob,
+    `nombre`      varchar(255) NOT NULL,
+    `peso`        float        NOT NULL,
+    `provincia`   varchar(255) NOT NULL,
+    `idraza`      int          NOT NULL,
+    `protectora`  varchar(255) NOT NULL,
+    PRIMARY KEY (`idmascota`),
+    KEY           `idraza_fk_index` (`idraza`),
+    KEY           `protectora_fk_index` (`protectora`),
+    CONSTRAINT `protectora_fk` FOREIGN KEY (`protectora`) REFERENCES `usuarios` (`email`),
+    CONSTRAINT `idraza_fk` FOREIGN KEY (`idraza`) REFERENCES `razas` (`idraza`)
+);
+
+DROP TABLE IF EXISTS `razas`;
+CREATE TABLE `razas`
+(
+    `idraza`  int NOT NULL AUTO_INCREMENT,
+    `nombre`  varchar(255) NOT NULL,
+    `especie` enum('PERRO','GATO','PAJARO') NOT NULL,
+    PRIMARY KEY (`idraza`)
+);
+
+DROP TABLE IF EXISTS `solicitud`;
+CREATE TABLE `solicitud`
+(
+    `idsolicitud` int NOT NULL AUTO_INCREMENT,
+    `alergias`    varchar(255) NOT NULL,
+    `estado`      enum('PENDIENTE','ACEPTADA','RECHAZADA', 'CANCELADA') NOT NULL,
+    `familia`     varchar(255) DEFAULT NULL,
+    `fecha`       datetime(6) NOT NULL,
+    `tipoHogar`   varchar(255) NOT NULL,
+    `idmascota`   int          NOT NULL,
+    `email`       varchar(255) NOT NULL,
+    PRIMARY KEY (`idsolicitud`),
+    KEY           `idmascota_fk_index` (`idmascota`),
+    KEY           `email_fk_index` (`email`),
+    CONSTRAINT `email_fk` FOREIGN KEY (`email`) REFERENCES `usuarios` (`email`),
+    CONSTRAINT `idmascota_fk` FOREIGN KEY (`idmascota`) REFERENCES `mascotas` (`idmascota`)
+);
+
+DROP TABLE IF EXISTS `usuarios`;
+CREATE TABLE `usuarios`
+(
+    `email`       varchar(255) NOT NULL,
+    `apellidos`   varchar(255) NOT NULL,
+    `cumpleanio`  date         NOT NULL,
+    `description` varchar(255) DEFAULT NULL,
+    `enabled`     boolean      DEFAULT true,
+    `nombre`      varchar(255) NOT NULL,
+    `password`    varchar(255) NOT NULL,
+    `provincia`   varchar(255) NOT NULL,
+    `telefono`    varchar(255) DEFAULT NULL,
+    `idrol`       int          NOT NULL,
+    PRIMARY KEY (`email`),
+    KEY           `idrol_fk_index` (`idrol`),
+    CONSTRAINT `idrol_fk` FOREIGN KEY (`idrol`) REFERENCES `roles` (`idrol`)
+);
+
+SET FOREIGN_KEY_CHECKS=1;
---
+++ backend/bin/src/main/resources/sql/02_master_tables.sql
@@ -0,0 +1,43 @@
+USE `adoptafur`;
+
+INSERT INTO `razas`
+VALUES (1, 'Bulldog Francés', 'PERRO'),
+       (2, 'Pastor Alemán', 'PERRO'),
+       (3, 'Labrador Retriever', 'PERRO'),
+       (4, 'Persa', 'GATO'),
+       (5, 'Siamés', 'GATO'),
+       (6, 'Bengala', 'GATO'),
+       (7, 'Canario', 'PAJARO'),
+       (8, 'Periquito', 'PAJARO'),
+       (9, 'Cacatúa', 'PAJARO'),
+       (10, 'Golden Retriever', 'PERRO'),
+       (11, 'Poodle', 'PERRO'),
+       (12, 'Beagle', 'PERRO'),
+       (13, 'Chihuahua', 'PERRO'),
+       (14, 'Yorkshire Terrier', 'PERRO'),
+       (15, 'Boxer', 'PERRO'),
+       (16, 'Dachshund', 'PERRO'),
+       (17, 'Mestiza', 'PERRO'),
+       (18, 'Otras', 'PERRO'),
+       (19, 'Maine Coon', 'GATO'),
+       (20, 'Ragdoll', 'GATO'),
+       (21, 'Sphynx', 'GATO'),
+       (22, 'Británico de Pelo Corto', 'GATO'),
+       (23, 'Abisinio', 'GATO'),
+       (24, 'Birmano', 'GATO'),
+       (25, 'Scottish Fold', 'GATO'),
+       (26, 'Mestiza', 'GATO'),
+       (27, 'Otras', 'GATO'),
+       (28, 'Diamante Mandarín', 'PAJARO'),
+       (29, 'Jilguero', 'PAJARO'),
+       (30, 'Agapornis', 'PAJARO'),
+       (31, 'Ninfa', 'PAJARO'),
+       (32, 'Loro Gris', 'PAJARO'),
+       (33, 'Guacamayo', 'PAJARO'),
+       (34, 'Cotorra', 'PAJARO'),
+       (35, 'Mestiza', 'PAJARO'),
+       (36, 'Otras', 'PAJARO');
+
+INSERT INTO `roles`
+VALUES (1, 'Usuario'),
+       (2, 'Protectora');
