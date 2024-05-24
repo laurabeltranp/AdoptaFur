7efff1ef8fd75d407de4961c5412cc4f8d42fe86
Laura <laurabeltranp15@gmail.com>
2024-05-24 12:47:13 +0200

    cambios estilos forms, politica y aviso, footer

--- frontend/app/cookies/page.js
+++ frontend/app/cookies/page.js
@@ -5,11 +5,26 @@ export default function Cookies() {
 
 
   return (
+    <main className="cookie-main">
+    <section>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" preserveAspectRatio="none">
+        <defs>
+          <linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="0%">
+            <stop offset="0%" style={{ stopColor: 'rgb(88,213,92)', stopOpacity: 0 }} />
+            <stop offset="100%" style={{ stopColor: 'rgb(88,213,92)', stopOpacity: 1 }} />
+          </linearGradient>
+          <linearGradient id="grad2" x1="0%" y1="0%" x2="100%" y2="0%">
+            <stop offset="0%" style={{ stopColor: 'rgb(255,255,255)', stopOpacity: 0 }} />
+            <stop offset="100%" style={{ stopColor: 'rgb(177,215,182)', stopOpacity: 1 }} />
+          </linearGradient>
+        </defs>
+        <path className="elementor-shape-fill" opacity="0.33" d="M473,67.3c-203.9,88.3-263.1-34-320.3,0C66,119.1,0,59.7,0,59.7V0h1000v59.7 c0,0-62.1,26.1-94.9,29.3c-32.8,3.3-62.8-12.3-75.8-22.1C806,49.6,745.3,8.7,694.9,4.7S492.4,59,473,67.3z" fill="url(#grad1)"></path>
+        <path className="elementor-shape-fill" opacity="0.66" d="M734,67.3c-45.5,0-77.2-23.2-129.1-39.1c-28.6-8.7-150.3-10.1-254,39.1 s-91.7-34.4-149.2,0C115.7,118.3,0,39.8,0,39.8V0h1000v36.5c0,0-28.2-18.5-92.1-18.5C810.2,18.1,775.7,67.3,734,67.3z" fill="url(#grad2)"></path>
+      </svg>
     <div className="container cookie-container mt-5 mb-4">
     <title>Aviso de Cookies - AdoptaFur</title>
 
     <div className="card p-5 shadow">
-      <main className="cookie-main">
         <h1 className="cookie-title">Política de Cookies</h1>
         <p className="cookie-description">
            AdoptaFur como  titular de este portal web, puede utilizar cookies propias y de terceros para distintas finalidades. Te informaremos y ofreceremos opciones de configuración sobre el uso de cookies tanto en la primera conexión a nuestro portal web como en cualquier momento posterior que lo desees mediante las opciones que ponemos a tu disposición.
@@ -74,8 +89,24 @@ export default function Cookies() {
         <p>
           A continuación, se muestra una lista de las cookies utilizadas en nuestro portal web.
         </p>
-      </main>
+
     </div>
   </div>
+  </section>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" preserveAspectRatio="none">
+      <defs>
+        <linearGradient id="grad1" x1="100%" y1="0%" x2="0%" y2="0%">
+          <stop offset="0%" style={{ 'stopColor': 'rgb(88,213,92)', 'stopOpacity': '0' }} />
+          <stop offset="100%" style={{ 'stopColor': 'rgb(88,213,92)', 'stopOpacity': '1' }} />
+        </linearGradient>
+        <linearGradient id="grad2" x1="100%" y1="0%" x2="0%" y2="0%">
+          <stop offset="0%" style={{ 'stopColor': 'rgb(255,255,255)', 'stopOpacity': '0' }} />
+          <stop offset="100%" style={{ 'stopColor': 'rgb(177,215,182)', 'stopOpacity': '1' }} />
+        </linearGradient>
+      </defs>
+      <path className="elementor-shape-fill" opacity="0.33" d="M527,67.3c203.9,88.3,263.1-34,320.3,0c78.8-49.3,144.8-108.7,144.8-108.7V1000H0V451.3 c0,0,62.1-26.1,94.9-29.3c32.8-3.3,62.8,12.3,75.8,22.1c19.5,9.5,80.2,50.4,130.6,54.4c51.5,4.1,244-49.2,263.4-57.5 C945.5,409.5,1000,336,1000,336H0c0,0,9.9,40.9,83.6,48.1C176.9,393.9,211.3,67.3,527,67.3z" fill="url(#grad1)"></path>
+      <path className="elementor-shape-fill" opacity="0.66" d="M266,67.3c45.5,0,77.2-23.2,129.1-39.1c28.6-8.7,150.3-10.1,254,39.1 s91.7-34.4,149.2,0C884.3,118.3,1000,39.8,1000,39.8V1000H0V963.5c0,0,28.2-18.5,92.1-18.5C189.8,945.9,224.3,67.3,266,67.3z" fill="url(#grad2)"></path>
+    </svg>
+</main>
 );
 }
--- frontend/app/politica/page.js
+++ frontend/app/politica/page.js
@@ -3,139 +3,227 @@ import styles from "./politica.css";
 
 export default function Politica() {
   return (
-    <div className={`container ${styles.container}`}>
-      <title>Política de Privacidad - AdoptaFur</title>
+    <main className="cookie-main">
+    <section>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1000 100"
+        preserveAspectRatio="none"
+      >
+        <defs>
+          <linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="0%">
+            <stop
+              offset="0%"
+              style={{ stopColor: "rgb(88,213,92)", stopOpacity: 0 }}
+            />
+            <stop
+              offset="100%"
+              style={{ stopColor: "rgb(88,213,92)", stopOpacity: 1 }}
+            />
+          </linearGradient>
+          <linearGradient id="grad2" x1="0%" y1="0%" x2="100%" y2="0%">
+            <stop
+              offset="0%"
+              style={{ stopColor: "rgb(255,255,255)", stopOpacity: 0 }}
+            />
+            <stop
+              offset="100%"
+              style={{ stopColor: "rgb(177,215,182)", stopOpacity: 1 }}
+            />
+          </linearGradient>
+        </defs>
+        <path
+          className="elementor-shape-fill"
+          opacity="0.33"
+          d="M473,67.3c-203.9,88.3-263.1-34-320.3,0C66,119.1,0,59.7,0,59.7V0h1000v59.7 c0,0-62.1,26.1-94.9,29.3c-32.8,3.3-62.8-12.3-75.8-22.1C806,49.6,745.3,8.7,694.9,4.7S492.4,59,473,67.3z"
+          fill="url(#grad1)"
+        ></path>
+        <path
+          className="elementor-shape-fill"
+          opacity="0.66"
+          d="M734,67.3c-45.5,0-77.2-23.2-129.1-39.1c-28.6-8.7-150.3-10.1-254,39.1 s-91.7-34.4-149.2,0C115.7,118.3,0,39.8,0,39.8V0h1000v36.5c0,0-28.2-18.5-92.1-18.5C810.2,18.1,775.7,67.3,734,67.3z"
+          fill="url(#grad2)"
+        ></path>
+      </svg>
+      <div className="container cookie-container mt-5 mb-4">
+        <title>Aviso Legal - AdoptaFur</title>
+        <div className="card p-5 shadow">
+          <h1 className="cookie-title">Política de Privacidad</h1>
+          <p className="cookie-description">
+            En AdoptaFur, como los jefes de este sitio web, queremos que estés
+            tranquilo sabiendo que nos tomamos muy en serio tu privacidad y la
+            seguridad de tus datos personales. Todo esto, en línea con las leyes
+            (UE) 2016/679 y Ley Orgánica 3/2018, para que estés al tanto de cómo
+            manejamos tus datos y tus derechos digitales.
+          </p>
+          <h2>Principios</h2>
+          <p className="cookie-description">
+            <strong>TRANSPARENCIA</strong>
+            <br />
+            Trataremos tus datos de forma segura y sin ningún tipo de fin oscuro
+            o perverso. Cuando recabemos tus datos personales a través de
+            nuestro Sitio Web serás informado convenientemente sobre: quién
+            trata tus datos, cuál es la finalidad del tratamiento, en qué se
+            basa la legitimidad del tratamiento, la posibilidad de ejercer tus
+            derechos y otra información de interés.
+          </p>
+          <p className="cookie-description">
+            <strong>CONTROL</strong>
+            <br />
+            Recabaremos tu consentimiento siempre que sea necesario y te
+            proporcionaremos las herramientas necesarias para que puedas acceder
+            y actualizar tu información personal, así como decidir cómo
+            gestionar tus datos, seleccionando las finalidades para las que
+            serán tratados.
+          </p>
+          <p className="cookie-description">
+            <strong>SEGURIDAD</strong>
+            <br />
+            Queremos asegurarnos de que tus datos estén seguros y protegidos.
+            Por eso, nos esforzamos al máximo para evitar que se pierdan o se
+            utilicen de manera inapropiada. Adoptamos todas las medidas
+            necesarias para proteger tu información y nos comprometemos a
+            mantenerla en secreto.
+          </p>
+          <p className="cookie-description">
+            En AdoptaFur, nos tomamos muy en serio tu privacidad. Por eso,
+            protegemos tu información según nuestra política de privacidad. Si
+            tienes alguna pregunta al respecto, escríbenos a info@AdoptaFur.com.
+            Estaremos encantados de ayudarte.
+          </p>
+          <h2>1. ¿Quién es responsable del tratamiento de los datos?</h2>
+          <p className="cookie-description">
+            AdoptaFur, empresa con CIF M-1254866 y domicilio social en C. de
+            García Martín, 21, 28224 Pozuelo de Alarcón, Madrid (España), será
+            la responsable del tratamiento de tus datos de conformidad con lo
+            que te informamos en esta Política de Privacidad.
+          </p>
+          <p className="cookie-description">
+            Además, no contamos con un Delegado de Protección de Datos pero
+            siemopre que tengas alguna pregunta, duda o queja sobre cómo
+            tratamos tus datos, puedes ponerte en contacto con nostros
+            escribiendo a través de nuestro formulario de contacto.
+          </p>
+          <h2>2. ¿Con qué finalidad tratamos tus datos personales?</h2>
+          <p className="cookie-description">
+            En AdoptaFur tratamos la información que nos facilitas con las
+            siguientes finalidades:
+          </p>
+          <ul>
+            <li>
+              <strong>Respondiendo a tus preguntas y peticiones:</strong> Usamos
+              tus datos para ayudarte con cualquier pregunta que tengas o
+              solicitud de información que nos hagas, ya sea por correo
+              electrónico, teléfono o redes sociales.
+            </li>
+            <li>
+              <strong>Enviándote ofertas chulas:</strong> Si nos das tu ok, te
+              mandamos promociones sobre nuestros productos y servicios, así
+              como noticias interesantes que te pueden gustar.
+            </li>
+            <li>
+              <strong>Ayudándote a encontrar trabajo:</strong> Si nos mandas tu
+              currículum para trabajar con nosotros, lo revisamos y te
+              consideramos para nuestras vacantes.
+            </li>
+            <li>
+              <strong>Mejorando lo que ofrecemos:</strong> Analizamos cómo usas
+              nuestros servicios para hacerlos aún mejores.
+            </li>
+            <li>
+              <strong>Cumpliendo con las leyes:</strong> Respetamos todas las
+              reglas y leyes que nos afectan, incluyendo responder a demandas
+              judiciales o administrativas si hace falta.
+            </li>
+          </ul>
 
-      <main className={`main ${styles.main}`}>
-        <h1 className={`title ${styles.title}`}>Política de Privacidad</h1>
-        <p className={`description ${styles.description}`}>
-          En AdoptaFur, como los jefes de este sitio web, queremos que
-          estés tranquilo sabiendo que nos tomamos muy en serio tu privacidad y
-          la seguridad de tus datos personales. Todo esto, en línea con las
-          leyes (UE) 2016/679 y Ley Orgánica 3/2018, para que estés al tanto de
-          cómo manejamos tus datos y tus derechos digitales.
-        </p>
-        <h2>Principios</h2>
-        <p>
-          <strong>TRANSPARENCIA</strong> <br></br>
-          Trataremos tus datos de forma segura y sin ningún tipo de fin oscuro o
-          perverso. Cuando recabemos tus datos personales a través de nuestro
-          Sitio Web serás informado convenientemente sobre: quién trata tus
-          datos, cuál es la finalidad del tratamiento, en qué se basa la
-          legitimidad del tratamiento, la posibilidad de ejercer tus derechos y
-          otra información de interés.
-        </p>
-        <p>
-          <strong>CONTROL</strong> <br></br>
-          Recabaremos tu consentimiento siempre que sea necesario y te
-          proporcionaremos las herramientas necesarias para que puedas acceder y
-          actualizar tu información personal, así como decidir cómo gestionar
-          tus datos, seleccionando las finalidades para las que serán tratados.
-        </p>
-        <p>
-          <strong>SEGURIDAD</strong> <br></br>
-          Queremos asegurarnos de que tus datos estén seguros y protegidos. Por
-          eso, nos esforzamos al máximo para evitar que se pierdan o se utilicen
-          de manera inapropiada. Adoptamos todas las medidas necesarias para
-          proteger tu información y nos comprometemos a mantenerla en secreto.
-        </p>
-        <p>
-          En AdoptaFur, nos tomamos muy en serio tu privacidad. Por eso,
-          protegemos tu información según nuestra política de privacidad. Si
-          tienes alguna pregunta al respecto, escríbenos a info@AdoptaFur.com.
-          Estaremos encantados de ayudarte.
-        </p>
-        <h2>1. ¿Quién es responsable del tratamiento de los datos?</h2>
-        <p>
-          AdoptaFur, empresa con CIF M-1254866 y domicilio social en C. de
-          García Martín, 21, 28224 Pozuelo de Alarcón, Madrid (España), será la
-          responsable del tratamiento de tus datos de conformidad con lo que te
-          informamos en esta Política de Privacidad.
-        </p>
-        <p>
-          Además, no contamos con un Delegado de Protección de Datos pero
-          siemopre que tengas alguna pregunta, duda o queja sobre cómo tratamos
-          tus datos, puedes ponerte en contacto con nostros escribiendo a través
-          de nuestro formulario de contacto.
-        </p>
-        <h2>2. ¿Con qué finalidad tratamos tus datos personales?</h2>
-        <p>
-          En AdoptaFur tratamos la información que nos facilitas con las
-          siguientes finalidades:
-        </p>
-        <ul>
-          <li>
-            <strong>Respondiendo a tus preguntas y peticiones:</strong> Usamos
-            tus datos para ayudarte con cualquier pregunta que tengas o
-            solicitud de información que nos hagas, ya sea por correo
-            electrónico, teléfono o redes sociales.
-          </li>
-          <li>
-            <strong>Enviándote ofertas chulas:</strong> Si nos das tu ok, te
-            mandamos promociones sobre nuestros productos y servicios, así como
-            noticias interesantes que te pueden gustar.
-          </li>
-          <li>
-            <strong>Ayudándote a encontrar trabajo:</strong> Si nos mandas tu
-            currículum para trabajar con nosotros, lo revisamos y te
-            consideramos para nuestras vacantes.
-          </li>
-          <li>
-            <strong>Mejorando lo que ofrecemos:</strong> Analizamos cómo usas
-            nuestros servicios para hacerlos aún mejores.
-          </li>
-          <li>
-            <strong>Cumpliendo con las leyes:</strong> Respetamos todas las
-            reglas y leyes que nos afectan, incluyendo responder a demandas
-            judiciales o administrativas si hace falta.
-          </li>
-        </ul>
+          <h2>3. ¿Por cuánto tiempo conservaremos tus datos personales?</h2>
+          <p className="cookie-description">
+            Mantendremos tus datos mientras sean útiles para lo que los
+            recopilamos y para cumplir con la ley. Después de eso, si ya no los
+            necesitamos, los guardaremos en un lugar seguro. Solo podrán acceder
+            a ellos los jueces o autoridades competentes si lo requieren, y
+            luego los borraremos.
+          </p>
+          <p className="cookie-description">
+            ¿Con quién compartiremos tus datos? Pues principalmente con las
+            protectoras que trabajan con nosotros para ayudar a los peluditos.
+            También, si la ley lo manda, con las autoridades. Pero no te
+            preocupes, siempre mantenemos tus datos seguros y solo los
+            compartimos cuando es necesario.
+          </p>
 
-        <h2>3. ¿Por cuánto tiempo conservaremos tus datos personales?</h2>
-        <p>
-          Mantendremos tus datos mientras sean útiles para lo que los
-          recopilamos y para cumplir con la ley. Después de eso, si ya no los
-          necesitamos, los guardaremos en un lugar seguro. Solo podrán acceder a
-          ellos los jueces o autoridades competentes si lo requieren, y luego
-          los borraremos.
-        </p>
-        <p>
-          ¿Con quién compartiremos tus datos? Pues principalmente con las
-          protectoras que trabajan con nosotros para ayudar a los peluditos.
-          También, si la ley lo manda, con las autoridades. Pero no te
-          preocupes, siempre mantenemos tus datos seguros y solo los compartimos
-          cuando es necesario.
-        </p>
+          <h2>5. ¿Cuáles son tus derechos cuando nos facilitas tus datos?</h2>
+          <p className="cookie-description">Si eres el jefe de tus datos, tienes estos derechos:</p>
+          <ul>
+            <li>Ver tus datos personales.</li>
+            <li>Cambiar cualquier dato que esté mal.</li>
+            <li>Borrar tus datos si ya no los necesitamos para nada.</li>
+            <li>No dejar que toquen tus datos sin permiso.</li>
+            <li>No permitir que usen tus datos sin tu consentimiento.</li>
+            <li>Llevar tus datos contigo si te vas.</li>
+          </ul>
+          <p className="cookie-description">
+            Si quieres usar estos superpoderes, escríbenos a las direcciones que
+            te dimos arriba.
+          </p>
 
-        <h2>5. ¿Cuáles son tus derechos cuando nos facilitas tus datos?</h2>
-        <p>Si eres el jefe de tus datos, tienes estos derechos:</p>
-        <ul>
-          <li>Ver tus datos personales.</li>
-          <li>Cambiar cualquier dato que esté mal.</li>
-          <li>Borrar tus datos si ya no los necesitamos para nada.</li>
-          <li>No dejar que toquen tus datos sin permiso.</li>
-          <li>No permitir que usen tus datos sin tu consentimiento.</li>
-          <li>Llevar tus datos contigo si te vas.</li>
-        </ul>
-        <p>
-          Si quieres usar estos superpoderes, escríbenos a las direcciones que
-          te dimos arriba.
-        </p>
-
-        <h2>6. ¿Cómo hemos obtenido tus datos?</h2>
-        <p>
-          Los datos que manejamos en AdoptaFur los conseguimos directo de ti, ya
-          sea llenando formularios en nuestros sitios web o chateando contigo
-          por aquí.
-        </p>
-        <h2>7. ¡Mantente al Día!</h2>
-        <p>
-          Reservamos el derecho de cambiar esta Política de Privacidad cuando
-          nos dé la gana. Te sugerimos que le des un ojo de vez en cuando para
-          ver cómo andamos cuidando tus datos.
-        </p>
-        <p>La última vez que la movimos fue el 23 de Mayo de 2024.</p>
-      </main>
-    </div>
-  );
+          <h2>6. ¿Cómo hemos obtenido tus datos?</h2>
+          <p className="cookie-description">
+            Los datos que manejamos en AdoptaFur los conseguimos directo de ti,
+            ya sea llenando formularios en nuestros sitios web o chateando
+            contigo por aquí.
+          </p>
+          <h2>7. ¡Mantente al Día!</h2>
+          <p className="cookie-description">
+            Reservamos el derecho de cambiar esta Política de Privacidad cuando
+            nos dé la gana. Te sugerimos que le des un ojo de vez en cuando para
+            ver cómo andamos cuidando tus datos.
+          </p>
+          <p className="cookie-description">La última vez que la movimos fue el 23 de Mayo de 2024.</p>
+        </div>
+      </div>
+    </section>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 1000 100"
+      preserveAspectRatio="none"
+    >
+      <defs>
+        <linearGradient id="grad1" x1="100%" y1="0%" x2="0%" y2="0%">
+          <stop
+            offset="0%"
+            style={{ stopColor: "rgb(88,213,92)", stopOpacity: "0" }}
+          />
+          <stop
+            offset="100%"
+            style={{ stopColor: "rgb(88,213,92)", stopOpacity: "1" }}
+          />
+        </linearGradient>
+        <linearGradient id="grad2" x1="100%" y1="0%" x2="0%" y2="0%">
+          <stop
+            offset="0%"
+            style={{ stopColor: "rgb(255,255,255)", stopOpacity: "0" }}
+          />
+          <stop
+            offset="100%"
+            style={{ stopColor: "rgb(177,215,182)", stopOpacity: "1" }}
+          />
+        </linearGradient>
+      </defs>
+      <path
+        className="elementor-shape-fill"
+        opacity="0.33"
+        d="M527,67.3c203.9,88.3,263.1-34,320.3,0c78.8-49.3,144.8-108.7,144.8-108.7V1000H0V451.3 c0,0,62.1-26.1,94.9-29.3c32.8-3.3,62.8,12.3,75.8,22.1c19.5,9.5,80.2,50.4,130.6,54.4c51.5,4.1,244-49.2,263.4-57.5 C945.5,409.5,1000,336,1000,336H0c0,0,9.9,40.9,83.6,48.1C176.9,393.9,211.3,67.3,527,67.3z"
+        fill="url(#grad1)"
+      ></path>
+      <path
+        className="elementor-shape-fill"
+        opacity="0.66"
+        d="M266,67.3c45.5,0,77.2-23.2,129.1-39.1c28.6-8.7,150.3-10.1,254,39.1 s91.7-34.4,149.2,0C884.3,118.3,1000,39.8,1000,39.8V1000H0V963.5c0,0,28.2-18.5,92.1-18.5C189.8,945.9,224.3,67.3,266,67.3z"
+        fill="url(#grad2)"
+      ></path>
+    </svg>
+  </main>
+);
 }
